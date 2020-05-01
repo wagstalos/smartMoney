eed038ea66c4a6e6c55dfc8acadfa7f5d425ace6
Wagner Paulo <wagstalos@hotmail.com>
2020-05-01 10:10:20 -0300

    adicionando-geolocalizacao-no-aplicativo-parte-2

--- src/pages/NewEntry/NewEntryAddressPicker/index.js
+++ src/pages/NewEntry/NewEntryAddressPicker/index.js
@@ -2,28 +2,53 @@ import React from 'react';
 import {Alert, View, TouchableOpacity, StyleSheet} from 'react-native';
 
 import Geolocation from '@react-native-community/geolocation';
+import Geocoder from 'react-native-geocoding';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Colors from '../../../styles/Colors';
 
+const getLocation = (latitude, longitude) => {
+  Geocoder.init('AIzaSyAsIrHUXtFAXfJ5nYGGTal0MFFGCfH3ngM');
+
+  Geocoder.from({latitude, longitude})
+    .then(json => {
+      const formattedAddress = json.results[0].formatted_address;
+      Alert.alert('Endereço Formatado', formattedAddress);
+    })
+    .catch(error => {
+      console.error(
+        'NewEntryAddressPicker :: getLocation :: erro ao recuperar a localização',
+        error,
+      );
+      Alert.alert(
+        'Houve um erro ao recuperar sua posição, por favor, tenha certeza que autorizou este aplicativo',
+      );
+    });
+};
+
+const getPosition = () => {
+  Geolocation.getCurrentPosition(
+    pos => {
+      const latitude = pos.coords.latitude;
+      const longitude = pos.coords.longitude;
+
+      getLocation(latitude, longitude);
+    },
+    error => {
+      console.error(
+        'NewEntryAddressPicker :: getPosition :: erro ao recuperar a posição',
+        error,
+      );
+      Alert.alert(
+        'Houve um erro ao recuperar sua posição, por favor, tenha certeza que autorizou este aplicativo',
+      );
+    },
+  );
+};
+
 const NewEntryAddressPicker = () => {
   const onButtonPress = () => {
-    Geolocation.getCurrentPosition(
-      pos => {
-        const latitude = pos.coords.latitude;
-        const longitude = pos.coords.longitude;
-
-        const position = `Lat. ${latitude} - Lon. ${longitude}`;
-
-        Alert.alert('Posição', position);
-      },
-      error => {
-        console.error(
-          'NewEntryAddressPicker :: erro ao recuperar a posição',
-          error,
-        );
-      },
-    );
+    getPosition();
   };
 
   return (
